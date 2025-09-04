765202e6ebb3ca1f855be29d306754937f1bf11f
Hari Chandan Reddy <168167397+softwarechandhanreddy@users.noreply.github.com>
2025-09-04 16:47:13 +0530

    checkingnumber

---
+++ checknumber.java
@@ -0,0 +1,23 @@
+package com.codegnan.fundamentals;
+import java.util.Scanner;
+
+public class checknumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        if (number > 0) {
+            System.out.println(number + " is a positive number.");
+        } else if (number < 0) {
+            System.out.println(number + " is a negative number.");
+        } else {
+            System.out.println("The number is zero.");
+        }
+
+        sc.close();
+    }
+}
+
+
